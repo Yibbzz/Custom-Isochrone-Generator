be7523a198dee579c188ed1f59d7d62e15dfc33d
Nathan <nathanleeks@gmail.com>
2025-08-27 20:39:55 +0000

    fixed master.yaml copying

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py makemigrations myapp --no-input
 python manage.py migrate --no-input
 
 echo "Copying master.yaml to PVC..."
-cp /webapp/media/user_osm_files/master.yaml /webapp/myapp/media/user_osm_files/master.yaml
+cp /webapp/bootstrap/master.yaml /webapp/media/user_osm_files/master.yaml
 
 echo "Starting application..."
 exec "$@"
