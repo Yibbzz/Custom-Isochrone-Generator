9dee2be88680db35e6b0269a7bd2c6d8ec5d5c36
Nathan <nathanleeks@gamail.com>
2026-06-07 17:38:36 +0000

    changed to fix migrations

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
-
 echo "Running migrations..."
-python manage.py makemigrations --no-input
-python manage.py makemigrations myapp --no-input
 python manage.py migrate --no-input
-
 echo "Copying master.yaml to PVC..."
 cp /webapp/deploy/bootstrap/master.yaml /webapp/myapp/media/user_osm_files/master.yaml
-
 echo "Starting application..."
 exec "$@"
